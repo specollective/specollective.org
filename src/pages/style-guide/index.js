dcd0640ec91c8282c913cc0cdaae350a1f223580
Joe Torreggiani <joetorreggiani@gmail.com>
2021-10-30 18:50:08 -0400

    Get spec-ods working and add simple style guide page

---
+++ src/pages/style-guide/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+export default class StyleGuidePage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello world</h1>
+
+        <div>
+          <plain-button>Plain Button</plain-button>
+        </div>
+      </div>
+    )
+  }
+}
