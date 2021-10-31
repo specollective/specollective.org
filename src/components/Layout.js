673fd65ff3acb719ca48dcf18ca0023f16d76e3b
Joe Torreggiani <joetorreggiani@gmail.com>
2021-10-30 23:36:57 -0400

    Fix file naming

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Helmet } from 'react-helmet'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
-import './all.scss'
+import './all.sass'
 import useSiteMetadata from './SiteMetadata'
 import { withPrefix } from 'gatsby'
 
